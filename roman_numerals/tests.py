5c5cbde602b97e8d440a93e5c4978684f5a9355d
konradko <konrad.korzel@gmail.com>
2013-11-19 19:52:11 +0000

    Add unittest for roman_numerals

---
+++ roman_numerals/tests.py
@@ -0,0 +1,12 @@
+import unittest
+
+class TestTransformRomanNumerals(unittest.TestCase):
+
+    def test_transfrom_from_roman_numerals(self):
+        self.fail("Write the test")
+
+    def test_transform_to_roman_numerals(self):
+        self.fail("Write the test")
+
+if __name__ == '__main__':
+    unittest.main()
