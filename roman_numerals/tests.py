e2c218c725f9918aabbbef68aaed4abd65ef19db
konradko <konrad.korzel@gmail.com>
2013-11-19 22:02:02 +0000

    Add transform to arabic numeral test and working function

--- roman_numerals/tests.py
+++ roman_numerals/tests.py
@@ -1,12 +1,37 @@
 import unittest
+from roman_numerals_converter import get_arabic_numeral
 
 class TestTransformRomanNumerals(unittest.TestCase):
 
     def test_transfrom_from_roman_numerals(self):
-        self.fail("Write the test")
+        # Integer provided in Roman numeral gets transformed into Arabic numeral
 
+        roman_per_arabic = {1000: 'M',
+                           500: 'D',
+                           100: 'C',
+                           50: 'L',
+                           10: 'X',
+                           5: 'V',
+                           1: 'I',
+                           2: 'II',
+                           100: 'LL',
+                           900: 'CM',
+                           1801: 'MDCCCI',
+                           1988: 'MCMLXXXVIII',
+                           1999: 'MCMXCIX',
+                           2000: 'MM',
+                           2013: 'MMXIII'
+                           }
+
+
+        for arabic in roman_per_arabic:
+            output = get_arabic_numeral(roman_per_arabic[arabic])
+            self.assertEqual(output, arabic)
+
+        
     def test_transform_to_roman_numerals(self):
-        self.fail("Write the test")
+        output = ''
+        self.assertEqual(output, 'I')
 
 if __name__ == '__main__':
     unittest.main()
