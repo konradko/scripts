a069bea3789b37d9f0536fa05e0522ac1de381cc
konradko <konrad.korzel@gmail.com>
2013-11-20 01:11:17 +0000

    Add Arabic to Roman numeral tests and functions

--- roman_numerals/roman_numerals_converter.py
+++ roman_numerals/roman_numerals_converter.py
@@ -4,36 +4,60 @@
 # numeral.
 # There should be one line of output for each line of input, containing the original number in the 
 # opposite format.
+class RomanNumeralsConverter:
+
+    def __init__(self):
+        self.roman_to_arabic = {'M': 1000,
+                               'D': 500,
+                               'C': 100,
+                               'L': 50,
+                               'X': 10,
+                               'V': 5,
+                               'I': 1}
+
+    def arabic_to_roman(self, arabic):
+        for roman in self.roman_to_arabic:
+            if self.roman_to_arabic[roman] == arabic:
+                return roman
+
+    def get_next_symbol(self, roman_symbol):
+        sorted_values = sorted(self.roman_to_arabic.values(), reverse=True)
+        next_arabic = sorted_values[sorted_values.index(self.roman_to_arabic[roman_symbol]) - 1]
+        return self.arabic_to_roman(next_arabic)
 
 
-def get_broken_int(number):
-    b1 = number % 10
-    b2 = (number % 100) / 10 * 10
-    b3 = (number % 1000) / 100 * 100
-    b4 = (number % 10000) / 1000 * 1000
-    return [b4, b3, b2, b1]
-
-def get_arabic_numeral(input_number):
-    arabic_numeral = 0
-    if not isinstance(input_number, int):
+    def get_arabic_numeral(self, input_number):
+        arabic_numeral = 0
         previous = 0
         for char in input_number:
-            # if the symbol appears before a larger symbol it is subtracted
-            current = mapping[char]
-
+            # if the symbol appears before a larger symbol it is substracted
+            current = self.roman_to_arabic[char]
             if previous >= current:
                 arabic_numeral += current
+            # When a symbol appears after a larger symbol it is added
             else:
                 arabic_numeral += current - previous * 2
-
             previous = current
-
         return arabic_numeral
 
-mapping = {'M': 1000,
-           'D': 500,
-           'C': 100,
-           'L': 50,
-           'X': 10,
-           'V': 5,
-           'I': 1}
+    def substract_roman(self, roman_string):
+        for roman in self.roman_to_arabic:
+            four_in_row = 4 * roman
+            if four_in_row in roman_string:
+                symbol_to_the_left = roman_string[roman_string.find(four_in_row) - 1]
+                next_symbol = self.get_next_symbol(roman)
+                if self.roman_to_arabic[symbol_to_the_left] > self.roman_to_arabic[next_symbol]:
+                    substracted = roman + next_symbol
+                    roman_string = roman_string.replace(four_in_row, substracted)
+                else:
+                    substracted = roman + self.get_next_symbol(next_symbol)
+                    roman_string = roman_string.replace(next_symbol + four_in_row, substracted)
+        return roman_string
+
+    def get_roman_numeral(self, arabic_number):
+        roman_string = ''
+        for arabic in sorted(self.roman_to_arabic.values(), reverse=True):
+            while arabic_number >= arabic:
+                arabic_number -= arabic
+                roman_string += self.arabic_to_roman(arabic)
+        return self.substract_roman(roman_string)
--- roman_numerals/tests.py
+++ roman_numerals/tests.py
@@ -1,37 +1,41 @@
 import unittest
-from roman_numerals_converter import get_arabic_numeral
+from roman_numerals_converter import RomanNumeralsConverter
 
-class TestTransformRomanNumerals(unittest.TestCase):
+class TestConvertRomanNumerals(unittest.TestCase):
+
+    def __init__(self, *args, **kwargs):
+        super(TestConvertRomanNumerals, self).__init__(*args, **kwargs)
+        
+        self.arabic_to_roman = {1000: 'M',
+                                500: 'D',
+                                100: 'C',
+                                50: 'L',
+                                10: 'X',
+                                5: 'V',
+                                1: 'I',
+                                2: 'II',
+                                900: 'CM',
+                                1801: 'MDCCCI',
+                                1988: 'MCMLXXXVIII',
+                                1999: 'MCMXCIX',
+                                2000: 'MM',
+                                2013: 'MMXIII'
+                                }
+    
+    def setUp(self):
+        self.converter = RomanNumeralsConverter()
 
     def test_transfrom_from_roman_numerals(self):
-        # Integer provided in Roman numeral gets transformed into Arabic numeral
-
-        roman_per_arabic = {1000: 'M',
-                           500: 'D',
-                           100: 'C',
-                           50: 'L',
-                           10: 'X',
-                           5: 'V',
-                           1: 'I',
-                           2: 'II',
-                           100: 'LL',
-                           900: 'CM',
-                           1801: 'MDCCCI',
-                           1988: 'MCMLXXXVIII',
-                           1999: 'MCMXCIX',
-                           2000: 'MM',
-                           2013: 'MMXIII'
-                           }
-
-
-        for arabic in roman_per_arabic:
-            output = get_arabic_numeral(roman_per_arabic[arabic])
+        # Integer provided in Roman numeral gets converted into Arabic numeral
+        for arabic in self.arabic_to_roman:
+            output = self.converter.get_arabic_numeral(self.arabic_to_roman[arabic])
             self.assertEqual(output, arabic)
-
         
     def test_transform_to_roman_numerals(self):
-        output = ''
-        self.assertEqual(output, 'I')
+        # Integer provided in Arabic numeral gets converted into Roman numeral
+        for arabic, roman in self.arabic_to_roman.iteritems():
+            output = self.converter.get_roman_numeral(arabic)
+            self.assertEqual(output, roman)
 
 if __name__ == '__main__':
     unittest.main()
